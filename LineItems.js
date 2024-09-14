7f8fce6b0171aee932f918c4799d47e296c0e9ee
Sneha Rajan <155979864+Sneha603@users.noreply.github.com>
2024-09-14 22:01:37 +0530

    LineItems.js

---
+++ LineItems.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { FaTrashAlt } from "react-icons/fa";
+
+const LineItems = ({item , handleCheck , handledelete}) => {
+  return (
+    <li className="item" key={item.id}>
+       <input
+         type="checkbox"
+         onChange={() =>handleCheck(item.id)}
+         checked={item.checked}
+      />
+      <label
+         style ={(item.checked)? {textDecoration :'Line-through'} :null}
+         onDoubleClick ={() =>handleCheck(item.id)}> {item.item}
+      </label>
+      <FaTrashAlt 
+        role = "button"
+        onClick={() =>handledelete(item.id)}
+        tabIndex = "0"
+        aria-label={`Delete ${item.item}`}
+      />
+
+
+    </li>
+
+  )
+}
+
+export default LineItems
